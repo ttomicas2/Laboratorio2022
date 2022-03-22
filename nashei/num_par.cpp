24a30502865e7bd39846f8069a9bd180a4ecd537
ttomicas2 <alumno24.caserez.tomas@ipm.edu.ar>
2022-03-22 09:58:52 -0300

    numero par negativo

--- nashei/num_par.cpp
+++ nashei/num_par.cpp
@@ -4,17 +4,16 @@ int main(){
     float num;
     cout<<"ingrese un numero"<<endl;
     cin>>num;
-
-    while (num >= 1)
-        {
+    if (num < 0){
+        num = num * (-1);
+    }
+    while (num >= 1) {
             num = num-2;
         }
-    if (num == 0)
-    {
+    if (num == 0) {
         cout<<"es par"<<endl;
     }
-    else
-    {
+    else{
         cout<<"es impar"<<endl;
     }
 }  
