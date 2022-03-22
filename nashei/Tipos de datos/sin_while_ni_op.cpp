9912439c814cb6922cf812fb5cff312ff827c450
ttomicas2 <alumno24.caserez.tomas@ipm.edu.ar>
2022-03-22 12:14:10 -0300

    Tipos de datos

--- nashei/Tipos de datos/sin_while_ni_op.cpp
+++ nashei/Tipos de datos/sin_while_ni_op.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int num = 11;
-    int res = 2;
-    int division = (num / res); 
-    cout<<division<<endl;
-
+    int num;
+    cout<<"Ingrese un numero"<<endl;
+    cin>>num;
+    if (num<0){
+        num = num * (-1);
+    }
+    int division1 = (num / 2);
+    int division2 = ((num + 1) / 2); 
+    if (division1 == division2){
+        cout<<"Es par"<<endl;
+    }
+    else{
+        cout<<"es impar"<<endl;
+    }
 }
