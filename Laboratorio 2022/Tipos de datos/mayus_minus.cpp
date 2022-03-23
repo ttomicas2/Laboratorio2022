6870c02d78ed0a28d279e05e84b05155ddb8065c
ttomicas2 <101263010+ttomicas2@users.noreply.github.com>
2022-03-23 09:41:33 -0300

    Update mayus_minus.cpp

--- Laboratorio 2022/Tipos de datos/mayus_minus.cpp
+++ Laboratorio 2022/Tipos de datos/mayus_minus.cpp
@@ -2,17 +2,23 @@
 using namespace std;
 int main(){
     char caracter;
-    cout<<"ingrese un caracter"<<endl;
-    cin>>caracter;
-    int ASCII = caracter;
-    if (ASCII>64 && ASCII<91){
-        cout<<"es mayuscula"<<endl;
+    int contador=0;
+    int ASCII;
+    while (contador == 0){
+        cout<<"ingrese una letra"<<endl;
+        cin>>caracter;
+        ASCII = caracter;
+        if (ASCII>64 && ASCII<91){
+            cout<<"es mayuscula"<<endl;
+            contador = 1;
+        }
+        else if (ASCII>96 && ASCII<123){
+            cout<<"es minuscula"<<endl;
+            contador = 1;
+        }   
+        else{
+            cout<<"no es una letrarda anashe"<<endl;
+        }    
     }
-    else if (ASCII>96 && ASCII<123){
-        cout<<"es minuscula"<<endl;
-    }
-    else{
-        cout<<"no es una letrarda anashe"<<endl;
-    }    
 }    
     
