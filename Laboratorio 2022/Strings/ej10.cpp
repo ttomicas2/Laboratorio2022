aee88bb7349121bac07a4c468e6f2a251fd84797
TadeoLu <101262571+TadeoLu@users.noreply.github.com>
2022-03-30 20:03:51 -0300

    Ej10 terminado

--- Laboratorio 2022/Strings/ej10.cpp
+++ Laboratorio 2022/Strings/ej10.cpp
@@ -8,27 +8,28 @@ int main(){
     int posicion_actual = 0;
     int letras_correctas = 0;
     int posicion_letras = 0;
+    int contador = 0;
     cout<<"escriba una palabra"<<endl;
     cin>>palabra1;
     cout<<"escriba otra palabra"<<endl;
     cin>>palabra2;
     int cant_letras1 = palabra1.size();
     int cant_letras2 = palabra2.size();
-    while (posicion_actual < cant_letras1){
+    while (posicion_actual < cant_letras2){
         suma = palabra1;
-        while (posicion_letras < cant_letras1)
+        contador = 0;
+        while (contador < cant_letras1)
         {
             char letra = palabra2[posicion_letras];
-            suma[posicion_letras] = letra;
+            suma[contador] = letra;
             posicion_letras++;
-            cout<<suma<<endl;
+            contador++;
         }
         posicion_actual++; 
         if (suma == palabra1)
         {
             posicion_actual = posicion_actual + cant_letras2;
             correcto++;
-            cout<<suma<<endl;
         }
         else
         {
@@ -42,6 +43,5 @@ int main(){
     else
     {
         cout<<"La primer palabra no entra en la segunda"<<endl;
-        cout<<suma<<endl;
     }
 }
