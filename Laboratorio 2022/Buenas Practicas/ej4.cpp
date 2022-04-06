9033e0b87f928622234eae48473a02c2f974945b
TadeoLu <101262571+TadeoLu@users.noreply.github.com>
2022-04-05 22:53:05 -0300

    Buenas Practicas

--- Laboratorio 2022/Buenas Practicas/ej4.cpp
+++ Laboratorio 2022/Buenas Practicas/ej4.cpp
@@ -4,12 +4,28 @@ int main(){
     int n;
     int resultado = 1;
     int resultado2 = 1;
+    int contador = 0;
     cout<<"escriba un numero"<<endl;
     cin>>n;
     //secuencia de Fibbonacci
-    while (resultado < n && resultado2 < n){
-        resultado2 = resultado + resultado2;
-        resultado = resultado2 + resultado;
+    if (n > 0)
+    {
+        while (n - 2 > contador ){
+            resultado2 = resultado + resultado2;
+            resultado = resultado2 + resultado;
+            contador = contador + 2;
+        }
+        if (n == 1){
+            cout<<"1"<<endl;
+        }
+        else if (n%2 == 0){
+            cout<<resultado<<endl;
+        }
+        else{
+                cout<<resultado2<<endl;
+        }
     }
-    (n > resultado2) ? cout<<resultado<<endl : cout<<resultado2<<endl;
+    else{
+    cout<<"ingrese un numero natural y distinto de cero"<<endl;
+    }   
 }
