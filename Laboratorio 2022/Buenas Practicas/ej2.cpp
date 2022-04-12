ca2f387c719fff80736bd40789e074c6c2a3a3d8
ttomicas2 <alumno24.caserez.tomas@ipm.edu.ar>
2022-04-12 12:33:00 -0300

    Repaso evalucaion

--- Laboratorio 2022/Buenas Practicas/ej2.cpp
+++ Laboratorio 2022/Buenas Practicas/ej2.cpp
@@ -8,13 +8,13 @@ int main(){
     cin>>numero;
     //verificamos si el numero es natural
     if (numero >= 0){
-        while (ciclos < numero){
+        while (ciclos <= numero){
             if (numero%ciclos == 0){
                 correcto++;
             }
             ciclos++;
         }
-        (correcto >= 2) ? cout<<"El numero no es primo"<<endl : cout<<"El numero es primo"<<endl;
+        (correcto > 2) ? cout<<"El numero no es primo"<<endl : cout<<"El numero es primo"<<endl;
     }
     else{
         cout<<"ingrese un numero natural"<<endl;
