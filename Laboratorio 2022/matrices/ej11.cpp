b4670131c1df744318de11536d92dc9d12d43d81
ttomicas2 <alumno24.caserez.tomas@ipm.edu.ar>
2022-09-28 09:54:33 -0300

    Ej11 matrices bro ashee

--- Laboratorio 2022/matrices/ej11.cpp
+++ Laboratorio 2022/matrices/ej11.cpp
@@ -2,7 +2,10 @@
 #include <vector>
 #include <string.h>
 using namespace std;
-
+struct posmat{
+    int x;
+    int y;
+}
 struct fecha{
     int dia;
     int mes;
@@ -24,9 +27,28 @@ struct producto{
     int codigo;
     int cant_vendida;   
 };
+void mostrar (vector<vector<int>>mat){
+    for(int i = 0; i < mat.size(); i++){
+        for(int j = 0; j < mat[i].size(); j++){
+            cout<<mat[i][j];
+        }
+        cout<<endl;
+    }
+}
+void mostrar_productos(vector<producto>productos){
+    for(int i = 0; i < productos.size(); i++){
+        cout<<productos[i].codigo<<endl;
+    }
+}
+void mostrar_empleados(vector<empleado>empleados){
+    for(int i = 0; i < empleados.size(); i++){
+        cout<<empleados[i].nombre<<endl;
+    }
+}
 //EMPLEADOS//
-void nuevo_empleado(vector<empleado>&empleados, empleado empleado_variable, vector<vector<int>>&matriz){
+void nuevo_empleado(vector<empleado>&empleados, vector<vector<int>>&matriz){
     vector<int>v1;
+    empleado empleado_variable;
     cout<<"ingrese nombre del empleado"<<endl;
     cin>> empleado_variable.nombre;
     cout<<"ingrese apellido del empleado"<<endl;
@@ -50,18 +72,22 @@ void nuevo_empleado(vector<empleado>&empleados, empleado empleado_variable, vect
     v1.push_back(empleado_variable.dni);
     matriz.push_back(v1);
     empleados.push_back(empleado_variable);
+    mostrar(matriz);
+    mostrar_empleados(empleados);
 }
 
-void modificar_empleado(empleado empleado_variable, vector<vector<int>>&matriz, vector<empleado>empleados){
+void modificar_empleado( vector<vector<int>>&matriz, vector<empleado>&empleados){
     int dni_ingresado;
     int posicion;
     bool esta = false;
     int n;
+    int p;
     cout<<"Ingrese dni del empleado que busca modificar"<<endl;
     cin>>dni_ingresado;
     for(int i = 0; i < matriz.size(); i++){
         if(matriz[i][0] == dni_ingresado){
             esta = true;
+            p = i;
         }
     }
     
@@ -77,6 +103,7 @@ void modificar_empleado(empleado empleado_variable, vector<vector<int>>&matriz, 
         cout<<"3. Fecha de ingreso"<<endl;
         cout<<"4. fecha de nacimiento"<<endl;
         cout<<"5. Localidad"<<endl;
+        cout<<"6. Dni"<<endl;
         cin>>n;
         switch (n)
         {
@@ -107,6 +134,10 @@ void modificar_empleado(empleado empleado_variable, vector<vector<int>>&matriz, 
         case 5:
             cout<<"Ingresar localidad"<<endl;
             cin>>empleados[posicion].localidad;
+        case 6:
+            cout<<"Ingrese dni"<<endl;
+            cin>>empleados[posicion].dni;
+            matriz[p][0]=empleados[posicion].dni
         default:
             cout<<"El numero no es valido"<<endl;
             break;
@@ -115,36 +146,42 @@ void modificar_empleado(empleado empleado_variable, vector<vector<int>>&matriz, 
     else{
         cout<<"El empleado no existe"<<endl;
     }
+    mostrar(matriz);
+    mostrar_empleados(empleados);
 }
 
-void eliminar_empleado(empleado empleado_variable, vector<vector<int>>&matriz, vector<empleado>empleados){
+void eliminar_empleado( vector<vector<int>>&matriz, vector<empleado>&empleados){
     int dni;
     bool esta = false;
     cout<<"Ingrese dni del empleado que quiera eliminar"<<endl;
     cin>>dni;
     for(int i = 0; i < matriz.size(); i++){
         if(dni == matriz[i][0]){
-            matriz.erase(matriz.end() - (matriz.size() - i));
+            matriz.erase(matriz.end() - (matriz.end() - i));
             esta = true;
         }
     }
     if(esta == true){
         for(int i = 0; i < empleados.size(); i++){
             if(dni == empleados[i].dni){
-                empleados.erase(empleados.end() - (empleados.size() - i));
+                empleados.erase(empleados.end() - (empleados.end() - i));
             }
         }
-        
-        
-
     }
+    else{
+        cout<<"No se encontro un empleado con ese dni"<<endl;
+    }
+    mostrar(matriz);
+    mostrar_empleados(empleados);
 }
 
 //EMPLEADOS//
 
 //PRODUCTOS//
 
-void nuevo_producto(producto producto_variable){
+void nuevo_producto(vector<vector<int>>&matriz, vector<producto>&productos){
+    producto producto_variable;
+    vector<int>v1;
     cout<<"ingrese la cantidad vendida del producto"<<endl;
     cin>> producto_variable.cant_vendida;
     cout<<"ingrese el codigo del producto"<<endl;
@@ -153,20 +190,117 @@ void nuevo_producto(producto producto_variable){
     cin>> producto_variable.precio_compra;
     cout<<"ingrese precio de venta del producto"<<endl;
     cin>> producto_variable.precio_venta;
+    v1.push_back(producto_variable.codigo);
+    matriz.push_back(v1);
+    productos.push_back(producto_variable);
+    mostrar(matriz);
+    mostrar_productos(productos);
 }
 
-void modificar_producto(producto){
+void modificar_producto( vector<vector<int>>&matriz, vector<producto>&productos){
+    int codigo;
+    int posicion;
+    bool esta = false;
+    int n;
+    posmat p;
+    cout<<"Ingrese codigo del producto que busca modificar"<<endl;
+    cin>>codigo;
+    for(int i = 0; i < matriz.size(); i++){
+        for(int j = 0; j < matriz[i].size(); j++){
+            if(matriz[i][j] == codigo){
+                esta = true;
+                p.x=i;
+                p.y=j;
+            }
+        }
+    }
+    
+    if(esta == true){
+        for(int i = 0; i < productos.size(); i++){
+            if(codigo == productos[i].codigo){
+                posicion = i;
+            }
+        }
+        cout<<"que desea modificar"<<endl;
+        cout<<"1. Cantidad de ventas"<<endl;
+        cout<<"2. Codigo"<<endl;
+        cout<<"3. Precio de Compra"<<endl;
+        cout<<"4. Precio de Venta"<<endl;
+        cin>>n;
+        switch (n)
+        {
+        case 1:
+            cout<<"Ingrese Cantidad de ventas"<<endl;
+            cin>>productos[posicion].cant_vendida;
+            break;
+        case 2:
+            cout<<"Ingrese Codigo"<<endl;
+            cin>>productos[posicion].codigo;
+            matriz[p.x][p.y] = productos[posicion].codigo;
+            break;
+        case 3:
+            cout<<"Ingresar Precio de compra"<<endl;
+            cin>>productos[posicion].precio_compra;
 
+            break;
+        case 4:
+            cout<<"Ingresar Precio de venta"<<endl;
+            cin>>productos[posicion].precio_venta;
+            break;
+        default:
+            cout<<"El numero no es valido"<<endl;
+            break;
+        }
+    }
+    else{
+        cout<<"El empleado no existe"<<endl;
+    }
+    mostrar(matriz);
+    mostrar_productos(productos);
 }
 
-void eliminar_producto(producto){
-
+void eliminar_producto( vector<vector<int>>&matriz, vector<producto>&productos){
+    int codigo;
+    vector<vector<int>>mat_temp;
+    vector<int>v1;
+    bool esta = false;
+    cout<<"Ingrese Codigo del producto que quiera eliminar"<<endl;
+    cin>>codigo;
+    for(int i = 0; i < matriz.size(); i++){
+        for(int j = 0; j < matriz[i].size(); j++ ){
+            if(codigo == matriz[i][j]){
+                esta = true;
+                matriz[i].erase(matriz[i].end() - (matriz[i].size() - j - 1));
+            }
+        }
+    }
+    if(esta == true){
+        for(int i = 0; i < productos.size(); i++){
+            if(codigo == productos[i].codigo){
+                productos.erase(productos.end() - (productos.end() - i));
+            }
+        }
+    }
+    else{
+        cout<<"No se encontro un producto con ese codigo"<<endl;
+    }
+    mostrar(matriz);
+    mostrar_productos(productos);
 }
 
 //PRODUCTO//
 
-void llenar_matriz(vector<vector<empleado>>matriz){
+void promedio(vector<vector<int>>&matriz, vector<producto>&productos){
+    float vueltas = 0;
+    float prom;
+    for(int i = 0; i < productos.size(); i++){
+        prom += (productos[i].precio_venta-productos[i].precio_compra);
+        vueltas++;
+    }
+    cout<<"La ganancia promedio es: "<<prom/vueltas<<endl;
+}
 
+void mayor_antiguedad(vector<vector<int>>&matriz, vector<producto>&productos){
 
 }
 
@@ -174,4 +308,99 @@ int main(){
     vector<vector<int>>matriz;
     vector<empleado>empleados;
     vector<producto>productos;
+    int menu = 0;
+    bool salir = false;
+    while(!salir){
+    cout<<"Ingrese una opcion"<<endl;
+    cout<<"1. Dar de alta nuevos producto, eliminar y modificarlos"<<endl;
+    cout<<"2. Dar de alta nuevos empleados, eliminar y modificarlos"<<endl;
+    cout<<"3. Calcular la ganancia promedio de los productos que se venden"<<endl;
+    cout<<"4. Nombre del empleado con mayor antigüedad en el trabajo junto con la fecha en que inició"<<endl;
+    cout<<"5. Código del producto que se vendió más"<<endl;
+    cout<<"6. Imprimir el dni, nombre de cada empleado junto con el nombre del último producto que se les añadió para vender"<<endl;
+    cout<<"7. Imprimir la fecha de nacimiento del empleado más grande en edad y el más joven"<<endl; 
+    cout<<"8. salir"<<endl;
+    cin>>menu;
+    switch (menu)
+    {
+    case 1:{
+        int caso;
+        bool salir = false;
+        while(!salir){
+            cout<<"Ingrese:"<<endl;
+            cout<<"1. Para dar de alta un producto"<<endl;
+            cout<<"2. para eliminar un producto"<<endl;
+            cout<<"3. modificar un producto"<<endl;
+            cout<<"4. salir"<<endl;
+            cin>>caso;
+            switch(caso){
+                case 1:
+                    nuevo_producto(matriz, productos);
+                    break;
+                case 2:
+                    eliminar_producto(matriz, productos);
+                    break;
+                case 3:
+                    modificar_producto(matriz, productos);
+                    break;
+                case 4:
+                    salir = true;
+                    break;
+                default:
+                    cout<<"ingrese una opcion valida"<<endl;
+                    break;
+            }
+        }
+        break;}
+    case 2:{
+        int caso;
+        bool salir = false;
+        while(!salir){
+            cout<<"Ingrese:"<<endl;
+            cout<<"1. Para dar de alta un empleado"<<endl;
+            cout<<"2. para eliminar un empleado"<<endl;
+            cout<<"3. modificar un empleado"<<endl;
+            cout<<"4. salir"<<endl;
+            cin>>caso;
+            switch(caso){
+                case 1:
+                    nuevo_empleado(empleados, matriz);
+                    break;
+                case 2:
+                    modificar_empleado(matriz, empleados);
+                    break;
+                case 3:
+                    eliminar_empleado(matriz, empleados);
+                    break;
+                case 4:
+                    salir = true;
+                    break;
+                default:
+                    cout<<"ingrese una opcion valida"<<endl;
+                    break;
+            }
+        }
+        break;}
+    case 3:
+        promedio(matriz, productos);
+        break;
+    case 4:
+
+        break;
+    case 5:
+
+        break;
+    case 6:
+
+        break;
+    case 7:
+
+        break;
+    case 8:
+        salir = true;
+        break;
+    default:
+        break;
+    }  
+    }
 }
--- Laboratorio 2022/matrices/prueba.cpp
+++ Laboratorio 2022/matrices/prueba.cpp
@@ -3,9 +3,22 @@
 using namespace std;
 
 int main(){
-    vector<int>enteros={1,2,3,4,5,6,7};
-    enteros.erase(enteros.end() - (enteros.size() - 3));
-    for(int i = 0; i < enteros.size(); i++){
-        cout<<enteros[i]<<endl;;
+    vector<vector<int>>matriz={           
+    {1,2,3},
+    {4,5,6},
+    {7,8,9} 
+    };
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cout<<matriz[i][j];
+        }
+        cout<<endl;
+    }
+    matriz[1].erase(matriz[1].end() - (matriz[1].end() - 1) );
+    for(int i = 0; i < matriz.size(); i++){
+        for(int j = 0; j < matriz[i].size(); j++){
+            cout<<matriz[i][j];
+        }
+        cout<<endl;
     }
 }
