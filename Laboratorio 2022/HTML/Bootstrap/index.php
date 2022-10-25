d30656b32911ef1240f79f78b213cf4c94c712f4
ttomicas2 <alumno24.caserez.tomas@ipm.edu.ar>
2022-10-25 12:36:35 -0300

    Petshop

--- Laboratorio 2022/HTML/Bootstrap/index.php
+++ Laboratorio 2022/HTML/Bootstrap/index.php
@@ -11,7 +11,7 @@
 <body>
     <nav class="navbar navbar-dark navbar-expand-lg bg-secondary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="index.php">
             <img src="PerroCats-naranja-azul.png" alt="" width="150" height="15%">
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,13 +20,13 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Perros</a>
+                <a class="nav-link" href="perros.php">Perros</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Gatos</a>
+                <a class="nav-link" href="gatos.php">Gatos</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- Laboratorio 2022/HTML/Bootstrap/perros.php
+++ Laboratorio 2022/HTML/Bootstrap/perros.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Pet Shop</title>
 </head>
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg bg-secondary">
+<nav class="navbar navbar-dark navbar-expand-lg bg-secondary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="index.php">
             <img src="PerroCats-naranja-azul.png" alt="" width="150" height="15%">
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,13 +20,13 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Perros</a>
+                <a class="nav-link" href="perros.php">Perros</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Gatos</a>
+                <a class="nav-link" href="gatos.php">Gatos</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
