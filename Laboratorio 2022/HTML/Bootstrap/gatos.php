9a34579f0dbf053301edc4bc37f178076816fd76
ttomicas2 <alumno24.caserez.tomas@ipm.edu.ar>
2022-11-01 12:31:54 -0300

    Pagina

--- Laboratorio 2022/HTML/Bootstrap/gatos.php
+++ Laboratorio 2022/HTML/Bootstrap/gatos.php
@@ -1,150 +1,163 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Pet Shop</title>
 </head>
+
 <body>
-<nav class="navbar navbar-dark navbar-expand-lg bg-secondary">
+    <nav class="navbar navbar-dark navbar-expand-lg bg-secondary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="index.php">
-            <img src="PerroCats-naranja-azul.png" alt="" width="150" height="15%">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="col align-self-centerlapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="col align-self-centerlapse navbar-col align-self-centerlapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="perros.php">Perros</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="gatos.php">Gatos</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Info
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Envios</a></li>
-                  <li><a class="dropdown-item" href="#">Ayuda</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Nosotros</a></li>
+            <a class="navbar-brand" href="index.php">
+                <img src="PerroCats-naranja-azul.png" alt="" width="150" height="15%">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="perros.php">Perros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="gatos.php">Gatos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Info
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Envios</a></li>
+                            <li><a class="dropdown-item" href="#">Ayuda</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Nosotros</a></li>
+                        </ul>
+                    </li>
                 </ul>
-              </li>
-            
-            </ul>
-          </div>
+            </div>
         </div>
-      </nav>
-<br>
-<div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-        <img src="Gatos/literaAzul.png" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Bandeja Con Filtro Litera Cerrada Para Gato Premium Trixie</h5>
-          <p class="card-text">$7.150,00</p>
-          <a href="#" class="btn btn-primary">Ver Producto</a>
+    </nav>
+    <br>
+    <div class="container text-center">
+        <div class="row align-items-center">
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/literaAzul.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Bandeja Con Filtro Litera Cerrada Para Gato Premium
+                            Trixie</h5>
+                        <p class="card-text">$7.150,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Agility Cats Kitten (Gato)</h5>
+                        <p class="card-text">$7.242,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/palitoRaton.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Juguete Gato Cañita Varita Cascabel Raton Plumas
+                            Extensible</h5>
+                        <p class="card-text">$383,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-        <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Agility Cats Kitten (Gato)</h5>
-          <p class="card-text">$7.242,00</p>
-          <a href="#" class="btn btn-primary">Ver Producto</a>
+        <br>
+
+        <div class="row align-items-center">
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/literaAzul.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Bandeja Con Filtro Litera Cerrada Para Gato Premium
+                            Trixie</h5>
+                        <p class="card-text">$7.150,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Agility Cats Kitten (Gato)</h5>
+                        <p class="card-text">$7.242,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Agility Cats Kitten (Gato)</h5>
+                        <p class="card-text">$7.242,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-      <img src="Gatos/palitoRaton.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Juguete Gato Cañita Varita Cascabel Raton Plumas Extensible</h5>
-        <p class="card-text">$383,00</p>
-        <a href="#" class="btn btn-primary">Ver Producto</a>
-      </div>
-    </div>
-  </div>
-</div>
-<br>
-<div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-        <img src="Gatos/literaAzul.png" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Bandeja Con Filtro Litera Cerrada Para Gato Premium Trixie</h5>
-          <p class="card-text">$7.150,00</p>
-          <a href="#" class="btn btn-primary">Ver Producto</a>
+        <br>
+        <div class="row align-items-center">
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/literaAzul.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Bandeja Con Filtro Litera Cerrada Para Gato Premium
+                            Trixie</h5>
+                        <p class="card-text">$7.150,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Agility Cats Kitten (Gato)</h5>
+                        <p class="card-text">$7.242,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col align-self-center">
+                <div class="card" style="width: 21rem;">
+                    <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title" style="height: 4rem;">Agility Cats Kitten (Gato)</h5>
+                        <p class="card-text">$7.242,00</p>
+                        <a href="#" class="btn btn-primary">Ver Producto</a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-        <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Agility Cats Kitten (Gato)</h5>
-          <p class="card-text">$7.242,00</p>
-          <a href="#" class="btn btn-primary">Ver Producto</a>
-        </div>
-      </div>
-    </div>
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-      <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Agility Cats Kitten (Gato)</h5>
-        <p class="card-text">$7.242,00</p>
-        <a href="#" class="btn btn-primary">Ver Producto</a>
-      </div>
-    </div>
-  </div>
-</div>
-<br>
-<div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-        <img src="Gatos/literaAzul.png" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Bandeja Con Filtro Litera Cerrada Para Gato Premium Trixie</h5>
-          <p class="card-text">$7.150,00</p>
-          <a href="#" class="btn btn-primary">Ver Producto</a>
-        </div>
-      </div>
-    </div>
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-        <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Agility Cats Kitten (Gato)</h5>
-          <p class="card-text">$7.242,00</p>
-          <a href="#" class="btn btn-primary">Ver Producto</a>
-        </div>
-      </div>
-    </div>
-    <div class="col align-self-center">
-      <div class="card" style="width: 18rem;">
-      <img src="Gatos/AgilityCats-Kitten-300x300.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Agility Cats Kitten (Gato)</h5>
-        <p class="card-text">$7.242,00</p>
-        <a href="#" class="btn btn-primary">Ver Producto</a>
-      </div>
-    </div>
-  </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
 </body>
