aa70d7fa431df72daa7299282a2b1efe4053eb1a
ttomicas2 <alumno24.caserez.tomas@ipm.edu.ar>
2022-11-08 12:34:13 -0300

    Pagina

---
+++ Laboratorio 2022/HTML/Bootstrap/producto.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Pet Shop</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #414042;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="PerroCats-naranja-azul.png" alt="" width="150" height="15%">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="perros.php">Perros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="gatos.php">Gatos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Info
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#Envios">Envios</a></li>
+                            <li><a class="dropdown-item" href="#Categorias">Categorias</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#Nosotros">Nosotros</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <img class="rounded">
+
+    </img>
+    <footer class="bd-footer " style="background-color: #414042;">
+        <div class="container text-center">
+            <div class="row aling-items-center">
+                <div class="col">
+                    <a name="Envios">
+                        <h2 class="text-white">Envios</h2>
+                    </a>
+                    <ul>
+                        <li>
+                            <p class="gris">Correo Argentino</p>
+                        </li>
+                        <li>
+                            <p class="gris">DHL</p>
+                        </li>
+                        <li>
+                            <p class="gris">FEDEX</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <a name="Categorias">
+                        <h2 class="text-white">Categorias</h2>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="perros.php" class="links">Perros</a>
+                        </li>
+                        <li>
+                            <a href="gatos.php" class="links">Gatos</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <a name="Nosotros">
+                        <h2 class="text-white">Nosotros</h2>
+                    </a>
+                    <ul>
+                        <li>
+                            <img src="logo-instagram.png" alt="" width="25px" height="25px">
+                            <a href="https://www.instagram.com/ttomicas/" class="links">@ttomicas</a>
+                        </li>
+                        <li>
+                            <img src="logo-instagram.png" alt="" width="25px" height="25px">
+                            <a href="https://www.instagram.com/tadecentrone/" class="links">@tadecentrone</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
